e109cf2fc0c9800fc8b981909007a73593b70019
RamaBotz19 <109959956+RamaBotz19@users.noreply.github.com>
2022-08-16 11:06:51 +0800

    Add files via upload

---
+++ plugins/donasi.js
@@ -0,0 +1,11 @@
+let handler = async m => m.reply(`
+╭─「 𝐃𝐨𝐧𝐚𝐬𝐢 𝐉𝐚𝐧𝐠𝐚𝐧 𝐌𝐚𝐢𝐧𝐢𝐧 𝐃𝐨𝐚𝐧𝐠:𝐯 」
+│ •⛽ 𝙂𝙤𝙥𝙖𝙮 [087793236775]
+│ •⛽ 𝙥𝙪𝙡𝙨𝙖 [082147562437]
+╰────
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
